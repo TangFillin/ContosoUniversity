6726c7b087da26dde16ddcf563f3d09275214526
collegetang@outlook.com <collegetang@outlook.com>
2020-04-21 21:40:31 +0800

    Intial Project

---
+++ ContosoUniversity/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoUniversity.Controllers
+{
+    
+    public class AccountController : Controller
+    {
+        private readonly SchoolContext _context;
+        public AccountController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Account/LoginViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models.Account
+{
+    public class LoginViewModel
+    {
+        #region Attribute
+
+        /// <summary>
+        /// 帐户名
+        /// </summary>
+        [Required(ErrorMessage = "登录名不能为空")]
+        [Display(Name = "登录名")]
+        public string Account { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        [Required(ErrorMessage = "密码不能为空")]
+        [DataType(DataType.Password)]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 是否记住帐户名
+        /// </summary>
+        [Display(Name = "记住登录名")]
+        public bool RememberMe { get; set; }
+
+        #endregion
+    }
+}
